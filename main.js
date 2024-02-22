98e23caba3416013a5c5b7dbc07a9c9b7509a007
by-yz <153606601+by-yz@users.noreply.github.com>
2024-02-22 14:06:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('#send_quizz1').click(function () {
+        var question = {
+            'valeur1': $('#question1').val(),
+            'valeur2': $("#code1").val(),
+
+
+        };
+        $.ajax({
+            url: 'http://127.0.0.1/submit.php',
+            type: 'POST',
+            dataType: 'json',
+            data: question,
+            success: function (data) {
+                if (data.succes) {
+                    //window.location.href = "index.html";
+                    console.log("transfert terminé. Valeurs reçues : " + (data));
+                }
+                else {
+                    console.log("force à toi bg")
+                }
+            },
+            error: function (xhr, status, error) {
+            }
+        });
+    });
+});
