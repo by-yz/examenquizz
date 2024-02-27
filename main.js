61eab4f4def7dc8564d26d0e7d62c24c54e03b4e
by-yz <153606601+by-yz@users.noreply.github.com>
2024-02-27 20:19:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -26,24 +26,3 @@ $(document).ready(function () {
     });
 });
 
-/////VERFICATION DU NOM///////
-$(document).ready(function () {
-    $("#verifierNom").click(function () {
-        var nomSaisi = $("#name_conn").val();
-
-        // Effectuer la requête Ajax
-        $.ajax({
-            type: "POST",
-            url: "http://127.0.0.1/db_connect.php", // Remplacez par le chemin de votre script PHP
-            dataType: "json",
-            data: { nom: nomSaisi },
-            success: function (result) {
-                // Afficher le résultat dans la div
-                $("#resultat").html(result);
-            },
-            error: function (error) {
-                console.error("Erreur lors de la requête Ajax : ", error);
-            }
-        });
-    });
-});
