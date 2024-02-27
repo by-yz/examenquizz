8910cd2216260990811f6b8405624c620b66bac6
by-yz <153606601+by-yz@users.noreply.github.com>
2024-02-27 19:05:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -25,3 +25,25 @@ $(document).ready(function () {
         });
     });
 });
+
+/////VERFICATION DU NOM///////
+$(document).ready(function () {
+    $("#verifierNom").on("click", function () {
+        var nomSaisi = $("#anme_conn").val();
+
+        // Effectuer la requête Ajax
+        $.ajax({
+            type: "POST",
+            url: "http://127.0.0.1/db_connect.php", // Remplacez par le chemin de votre script PHP
+            dataType: "json",
+            data: { nom: nomSaisi },
+            success: function (result) {
+                // Afficher le résultat dans la div
+                $("#resultat").html(result);
+            },
+            error: function (error) {
+                console.error("Erreur lors de la requête Ajax : ", error);
+            }
+        });
+    });
+});
