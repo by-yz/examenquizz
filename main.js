0bff61c623e468aff4f2d5ed07541081ede613da
by-yz <153606601+by-yz@users.noreply.github.com>
2024-02-28 10:32:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -25,4 +25,90 @@ $(document).ready(function () {
         });
     });
 });
+//question 2
+$(document).ready(function () {
+    $('#send_quizz2').click(function () {
+        var question = {
+            'valeur3': $('#question2').val(),
+            'valeur4': $("#name2").val(),
+
+
+        };
+        $.ajax({
+            url: 'http://127.0.0.1/submit.php',
+            type: 'POST',
+            dataType: 'json',
+            data: question,
+            success: function (data) {
+                if (data.succes) {
+                    //window.location.href = "index.html";
+                    console.log("transfert terminé. Valeurs reçues : " + (data));
+                }
+                else {
+                    console.log("force à toi bg")
+                }
+            },
+            error: function (xhr, status, error) {
+            }
+        });
+    });
+});
+// question 3
+$(document).ready(function () {
+    $('#send_quizz3').click(function () {
+        var question = {
+            'valeur5': $('#question3').val(),
+            'valeur6': $("#name3").val(),
+
+
+        };
+        $.ajax({
+            url: 'http://127.0.0.1/submit.php',
+            type: 'POST',
+            dataType: 'json',
+            data: question,
+            success: function (data) {
+                if (data.succes) {
+                    //window.location.href = "index.html";
+                    console.log("transfert terminé. Valeurs reçues : " + (data));
+                }
+                else {
+                    console.log("force à toi bg")
+                }
+            },
+            error: function (xhr, status, error) {
+            }
+        });
+    });
+});
+//question 4
+$(document).ready(function () {
+    $('#send_quizz4').click(function () {
+        var question = {
+            'valeur7': $('#question4').val(),
+            'valeur8': $("#name4").val(),
+
+
+        };
+        $.ajax({
+            url: 'http://127.0.0.1/submit.php',
+            type: 'POST',
+            dataType: 'json',
+            data: question,
+            success: function (data) {
+                if (data.succes) {
+                    //window.location.href = "index.html";
+                    console.log("transfert terminé. Valeurs reçues : " + (data));
+                }
+                else {
+                    console.log("force à toi bg")
+                }
+            },
+            error: function (xhr, status, error) {
+            }
+        });
+    });
+});
+
+
 
